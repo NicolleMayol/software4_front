a39d64c0b3f4f889114d2a890ac5c9ff839576e8
kevin1920 <56529861+kevin1920@users.noreply.github.com>
2021-03-25 16:14:39 -0500

    Se arreglo el mapa con las coordenadas

--- js/recorridos.js
+++ js/recorridos.js
@@ -4,29 +4,43 @@ let idLugar = localStorage.getItem('idLugar')
 let token = localStorage.getItem('info')
 let map;
 
-let guardarCoordenadas = () => {
-  axios.get(`http://localhost:3000/api/v1/coordenadas/${idLugar}`,{headers:{"token":token}}).then(respuesta => {
-      lat = respuesta.data[0].lat
-      long = respuesta.data[0].long
-      console.log(respuesta.data)
+
+let etiquetaNombre = () => {
+  axios.get(`http://localhost:3000/api/v1/lugares/${idLugar}`,{headers:{"token":token}}).then(respuesta => {
+      let nombre = document.getElementById('nonbreRecorrido')
+      nombre.innerHTML = respuesta.data[0].nombre
   }).catch(error => {
       console.log(error)
   })
 }
 
 function initMap() {
-  const myLatLng = { lat: lat, lng: long },
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: myLatLng,
-    zoom: 20,
-  });
+  axios.get(`http://localhost:3000/api/v1/coordenadas/${idLugar}`,{headers:{"token":token}}).then(respuesta => {
+      lat = respuesta.data[0].lat
+      long = respuesta.data[0].long
+      console.log(lat)
+      console.log(long)
 
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-  });
+       const myLatLng = { lat: lat, lng: long },
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: myLatLng,
+        zoom: 20,
+      });
 
+      console.log(myLatLng)
+
+      new google.maps.Marker({
+        position: myLatLng,
+        map,
+      });
+
+      etiquetaNombre();
+
+  }).catch(error => {
+      console.log(error)
+  })
+  
 }
 
-guardarCoordenadas();
+
 
