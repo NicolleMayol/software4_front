de815616004a650dd9609737bade2f7674253e26
Nicolle Mayol <42590549+NicolleMayol@users.noreply.github.com>
2021-04-20 16:42:57 -0500

    Agregado video y función para añadir marcas al map

--- js/recorridos.js
+++ js/recorridos.js
@@ -1,3 +1,5 @@
+
+
 let lat = 0
 let long = 0
 let idLugar = localStorage.getItem('idLugar')
@@ -6,41 +8,50 @@ let map;
 
 
 let etiquetaNombre = () => {
-  axios.get(`http://localhost:3000/api/v1/lugares/${idLugar}`,{headers:{"token":token}}).then(respuesta => {
-      let nombre = document.getElementById('nonbreRecorrido')
-      nombre.innerHTML = respuesta.data[0].nombre
+  axios.get(`http://localhost:3000/api/v1/lugares/${idLugar}`, { headers: { "token": token } }).then(respuesta => {
+    let nombre = document.getElementById('nonbreRecorrido')
+    nombre.innerHTML = respuesta.data[0].nombre
   }).catch(error => {
-      console.log(error)
+    console.log(error)
   })
 }
 
 function initMap() {
-  axios.get(`http://localhost:3000/api/v1/coordenadas/${idLugar}`,{headers:{"token":token}}).then(respuesta => {
-      lat = respuesta.data[0].lat
-      long = respuesta.data[0].long
-      console.log(lat)
-      console.log(long)
+  axios.get(`http://localhost:3000/api/v1/coordenadas/${idLugar}`, { headers: { "token": token } }).then(respuesta => {
+    lat = respuesta.data[0].lat
+    long = respuesta.data[0].long
+    console.log(lat)
+    console.log(long)
 
-       const myLatLng = { lat: lat, lng: long },
+    const myLatLng = { lat: lat, lng: long },
       map = new google.maps.Map(document.getElementById("map"), {
         center: myLatLng,
         zoom: 20,
+        mapId: "6cd337914d17672b"
       });
 
-      console.log(myLatLng)
+      addMarker(myLatLng, map, "Udem");
+    console.log(myLatLng)
 
-      new google.maps.Marker({
-        position: myLatLng,
-        map,
-      });
-
-      etiquetaNombre();
+    etiquetaNombre();
 
   }).catch(error => {
-      console.log(error)
+    console.log(error)
   })
-  
+
 }
 
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  new google.maps.Marker({
+    position: location,
+    label: label,
+    map: map,
+  });
+}
 
+google.maps.event.addDomListener(window, 'resize', initialize);
+google.maps.event.addDomListener(window, 'load', initialize)
 
